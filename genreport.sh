bcc103ccdf2876669df04f1ce43a1b10e21371c5
michael <mike@mike.com>
2014-12-11 11:03:10 +0200

    updates

--- genreport.sh
+++ genreport.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # get the name of the dataset
-# sed ...
-latex templates/template.latex
-bibtex templates/template.aux
-pdflatex templates/template.latex
+for dataset in `ls data/*.arff`
+do
+    dataset=$(basename $dataset)
+    dataset="${dataset%.*}"
+    cp template/template.latex sample/$dataset.latex
+    cp template/Master.bib sample/Master.bib
+    sed -i -e "s/@dataset@/${dataset}/g" sample/$dataset.latex
+    latex sample/$dataset.latex
+    bibtex sample/$dataset.aux
+    pdflatex sample/$dataset.latex
+done
